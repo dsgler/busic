556ea50f7a42b4911acb78e39be32a37f762621d
dsgler <1832002325@qq.com>
2026-01-30 12:05:50 +0800

    feat: add windows support

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:busic/network/request.dart';
 import 'package:busic/pages/music_list_page.dart';
 import 'package:busic/providers/user_info_provider.dart';
@@ -5,6 +7,8 @@ import 'package:busic/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:just_audio_media_kit/just_audio_media_kit.dart';
 // import './network/test.dart';
 
 void main() async {
@@ -21,6 +25,14 @@ void main() async {
   // });
 
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux) {
+    // 初始化 sqflite_common_ffi
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+
+    JustAudioMediaKit.ensureInitialized();
+  }
 
   // 初始化 SharedPreferences
   final sharedPreferences = await SharedPreferences.getInstance();
